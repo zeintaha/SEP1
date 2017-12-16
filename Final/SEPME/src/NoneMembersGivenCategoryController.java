5b9d431d9a5d77ee2371579829d5b21408e7b3d4
Taha <zeintaha@hotmail.com>
2017-12-17 00:57:08 +0100

    Adding Getting none member by given category

--- Final/SEPME/src/NoneMembersGivenCategoryController.java
+++ Final/SEPME/src/NoneMembersGivenCategoryController.java
@@ -1,54 +1,153 @@
-
-
-/**
- * Sample Skeleton for 'NoneMembersGivenCategory.fxml' Controller Class
- */
-
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
-public class NoneMembersGivenCategoryController {
+public class NoneMembersGivenCategoryController implements Initializable
+{
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+   private ArrayList<Non_Members> nonMemberArray = new ArrayList<Non_Members>();
+   private ToBinary file;
+   private String filename = "nonmember.txt";
+   private static ObservableList<Non_Members> observableNonMembers;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+   private ArrayList<Category> categories = new ArrayList<Category>();
 
-    @FXML // fx:id="tblColumnPhone"
-    private TableColumn<?, ?> tblColumnPhone; // Value injected by FXMLLoader
+   @FXML
+   private ResourceBundle resources;
 
-    @FXML // fx:id="tblColumnName"
-    private TableColumn<?, ?> tblColumnName; // Value injected by FXMLLoader
+   @FXML
+   private URL location;
 
-    @FXML // fx:id="tblNoneMember"
-    private TableView<?> tblNoneMember; // Value injected by FXMLLoader
+   @FXML
+   private AnchorPane AnchorPaneMembers;
 
-    @FXML // fx:id="tblColumnEmail"
-    private TableColumn<?, ?> tblColumnEmail; // Value injected by FXMLLoader
+   @FXML
+   private TableColumn<Non_Members, String> tblColumnPhone;
 
-    @FXML // fx:id="AnchorPaneNoneMembers"
-    private AnchorPane AnchorPaneNoneMembers; // Value injected by FXMLLoader
+   @FXML
+   private TableColumn<Non_Members, String> tblColumnName;
 
-    @FXML // fx:id="tblColumnAddress"
-    private TableColumn<?, ?> tblColumnAddress; // Value injected by FXMLLoader
+   @FXML
+   private ComboBox<Category> cmbPreferences;
 
-    @FXML // fx:id="tblColumnPreferences"
-    private TableColumn<?, ?> tblColumnPreferences; // Value injected by FXMLLoader
+   @FXML
+   private TableView<Non_Members> tblMember;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert tblColumnPhone != null : "fx:id=\"tblColumnPhone\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert tblColumnName != null : "fx:id=\"tblColumnName\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert tblNoneMember != null : "fx:id=\"tblNoneMember\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert tblColumnEmail != null : "fx:id=\"tblColumnEmail\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert AnchorPaneNoneMembers != null : "fx:id=\"AnchorPaneNoneMembers\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert tblColumnAddress != null : "fx:id=\"tblColumnAddress\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
-        assert tblColumnPreferences != null : "fx:id=\"tblColumnPreferences\" was not injected: check your FXML file 'NoneMembersGivenCategory.fxml'.";
+   @FXML
+   private TableColumn<Non_Members, String> tblColumnEmail;
 
-    }
+   @FXML
+   private TableColumn<Non_Members, String> tblColumnAddress;
+
+   @FXML
+   private TableColumn<Non_Members, String> tblColumnPreferences;
+
+   
+   @SuppressWarnings("unchecked")
+   @FXML
+   void preferences(ActionEvent event)
+   {
+      ArrayList<Non_Members> nonMembers = new ArrayList<Non_Members>();
+      nonMemberArray = (ArrayList<Non_Members>) file.readObjFromFile();
+      for (int i = 0; i < nonMemberArray.size(); i++)
+      {
+         if ((cmbPreferences.getSelectionModel().getSelectedItem().toString())
+               .equals("Show All"))
+         {
+            nonMembers.add(nonMemberArray.get(i));
+
+         }
+         if (nonMemberArray.get(i).getPreferences().toString().equals(
+               cmbPreferences.getSelectionModel().getSelectedItem().toString()))
+         {
+            nonMembers.add(nonMemberArray.get(i));
+         }
+      }
+      observableNonMembers = FXCollections
+            .observableList(nonMembers);
+      tblMember.setItems(observableNonMembers);
+      System.out.println();
+
+   }
+
+   @SuppressWarnings("unchecked")
+   public void initializeTable()
+   {
+      nonMemberArray = new ArrayList<Non_Members>();
+
+      tblColumnName.setCellValueFactory(
+            new PropertyValueFactory<Non_Members, String>("name"));
+      tblColumnAddress.setCellValueFactory(
+            new PropertyValueFactory<Non_Members, String>("address"));
+      
+      tblColumnEmail.setCellValueFactory(
+            new PropertyValueFactory<Non_Members, String>("phoneNr"));
+      tblColumnPhone.setCellValueFactory(
+            new PropertyValueFactory<Non_Members, String>("email"));
+      tblColumnPreferences.setCellValueFactory(
+            new PropertyValueFactory<Non_Members, String>("Preferences"));
+      
+      observableNonMembers = FXCollections
+            .observableList((ArrayList<Non_Members>) file.readObjFromFile());
+      tblMember.setItems(observableNonMembers);
+      tblMember.setStyle("-fx-alignment: CENTER;");
+   }
+
+   
+   
+   @FXML
+   void initialize()
+   {
+
+   }
+
+   @SuppressWarnings("unchecked")
+   private void showWrittenData()
+   {
+      ArrayList<Non_Members> list = (ArrayList<Non_Members>) file.readObjFromFile();
+      for (int i = 0; i < list.size(); i++)
+      {
+         System.out.println(list.get(i));
+      }
+   }
+
+   @SuppressWarnings("unchecked")
+   public void refreshComboBox()
+   {
+      categories = new ArrayList<Category>();
+
+      String category = "Show All";
+      Category cat = new Category(category);
+
+      categories = (ArrayList<Category>) file.readObjFromFile();
+      categories.add(cat);
+      cmbPreferences.getItems().addAll(categories);
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public void initialize(URL arg0, ResourceBundle arg1)
+   {
+      filename = "categories.txt";
+      file = new ToBinary(filename);
+      refreshComboBox();
+      filename = "nonmember.txt";
+      file = new ToBinary(filename);
+      nonMemberArray = new ArrayList<Non_Members>();
+      nonMemberArray = (ArrayList<Non_Members>) file.readObjFromFile();
+      initializeTable();
+      showWrittenData();
+   }
+
 }
